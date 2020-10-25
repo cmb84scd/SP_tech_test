a6c829a9ea256d8f55bbbe501e3233df4e6fe107
cmb84scd <cmb84scd@gmail.com>
2020-10-25 17:45:03 +0000

    Refactor tests to use before each block

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,23 +4,24 @@ describe Parser do
   let(:parser) { Parser.new }
   test_log = File.dirname(__FILE__) + '/test.log'
   
+  before(:each) do
+    parser.get_log_data(test_log)
+  end
+
   describe '#get_log_data' do
     it 'stores data as array of hashes' do
-      parser.get_log_data(test_log)
       expect(parser.site_visits).to include({ page: '/about/2', url: '444.701.448.104' })
     end
   end
 
   describe '#page_visit_count' do
     it 'sorts and counts the data' do
-      parser.get_log_data(test_log)
       expect(parser.page_visit_count).to eq([['/help_page/1', 6], ['/contact', 3], ['/home', 3], ['/about/2', 3], ['/index', 3], ['/about', 2]])
     end
   end
 
   describe '#print_page_visit_info' do
     it 'prints the page visit information' do
-      parser.get_log_data(test_log)
       expect(parser.print_page_visit_info).to eq ['/help_page/1 6 visits', '/contact 3 visits', '/home 3 visits', '/about/2 3 visits', '/index 3 visits', '/about 2 visits']
     end
   end
